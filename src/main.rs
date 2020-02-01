c1ba5a5e5d0deaad3c6c316a94eda97ec4b62006
Meenakshi Sundaram V <vms20591@riseup.net>
2020-02-01 15:40:32 +0530

    Implement add - json serialize bookmark

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,34 @@
 use clap::{Arg, App, SubCommand};
+use serde::{Serialize, Deserialize};
+use serde_json::{Result as SerdeResult};
 
-fn add_bookmark(name: &str, url: &str) {
-    println!("Adding bookmark {}: {}", name, url);
+#[derive(Serialize, Deserialize, Debug)]
+struct Bookmark {
+    name: String,
+    url: String
 }
 
-fn main() {
+impl Bookmark {
+    fn new(name: String, url: String) -> Bookmark {
+        Bookmark {name, url} 
+    }
+
+    fn to_json(&self) -> SerdeResult<String> {
+        Ok(serde_json::to_string_pretty(self)?)
+    }
+}
+
+fn add_bookmark(name: &str, url: &str) -> SerdeResult<()> {
+    let bookmark = Bookmark::new(String::from(name), String::from(url));
+
+    let json = bookmark.to_json()?;
+
+    println!("Bookmark: {}", json);
+
+    Ok(())
+}
+
+fn main() -> SerdeResult<()> {
     let matches = App::new("marktor")
         .version("1.0")
         .author("Meenakshi Sundaram V <vms20591@riseup.net>")
@@ -25,6 +49,8 @@ fn main() {
         let name = sub_match.value_of("name").unwrap();
         let url = sub_match.value_of("url").unwrap();
 
-        add_bookmark(name, url);
+        add_bookmark(name, url)?;
     }
+
+    Ok(())
 }
